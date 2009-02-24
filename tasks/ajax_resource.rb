1336b5833e04f215e82378ca76825b3663897952
ratnikov <ratnikov@gmail.com>
2009-02-23 21:22:31 -0600

    Changed 'install' task to compile the scripts anew.

--- tasks/ajax_resource.rb
+++ tasks/ajax_resource.rb
@@ -1,19 +1,22 @@
 
 namespace :ajax_resource do
   namespace :rails do
-    desc "Re-builds the library" 
-    task :build do
-      require 'jake'
-      Jake.build File.join(File.dirname(__FILE__), '..'), true
-    end
-
     desc "Installs ajax_resource javascript in the public/javascripts directory. Set COMPRESS=true to install the compressed version."
-    task :install => [ :build ] do
-      extension = ENV["COMPRESS"] == "true" ? "min" : "src"
-      from = File.join File.dirname(__FILE__), '..', 'build', "ajax_resource-#{extension}.js"
-      to = File.join Rails.root, 'public', 'javascripts', 'ajax_resource.js'
-      FileUtils.cp(from, to, :verbose => true);
-      puts "Done installing." 
+    task :install do
+      require 'jake'
+
+      build = Jake::Build.new File.join(File.dirname(__FILE__), '..')
+
+      class << build
+	def build_directory
+	  File.join Rails.root, 'public', 'javascripts'
+	end
+      end
+
+      build.force!
+      build.run!
+
+      puts "Installed to #{build.build_directory}"
     end
   end
 end
