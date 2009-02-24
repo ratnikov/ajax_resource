976d69af052828b9745b472b7f930f737a55a3ba
ratnikov <ratnikov@gmail.com>
2009-02-23 19:20:34 -0600

    Re-implemented builder to use jake ruby bindings.

--- tasks/ajax_resource.rb
+++ tasks/ajax_resource.rb
@@ -1,8 +1,14 @@
 
 namespace :ajax_resource do
   namespace :rails do
+    desc "Re-builds the library" 
+    task :build do
+      require 'jake'
+      Jake.build File.join(File.dirname(__FILE__), '..'), true
+    end
+
     desc "Installs ajax_resource javascript in the public/javascripts directory. Set COMPRESS=true to install the compressed version."
-    task :install do
+    task :install => [ :build ] do
       extension = ENV["COMPRESS"] == "true" ? "min" : "src"
       from = File.join File.dirname(__FILE__), '..', 'build', "ajax_resource-#{extension}.js"
       to = File.join Rails.root, 'public', 'javascripts', 'ajax_resource.js'
