ae5faef422d4d026843951886c4beebd549c8ff5
ratnikov <ratnikov@gmail.com>
2009-02-27 13:07:57 -0600

    Updated implementation to support non-array.

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,15 +1,21 @@
 
 function matches_only(actual, selectors, message) {
+  if (typeof actual === "undefined") {
+    ok(false, message + ": expected selector should be defined");
+    return;
+  }
+
   if (!jQuery.isArray(selectors)) {
     selectors = [ selectors ];
   }
 
+
   var unmatched = actual;
   jQuery.each(selectors, function() {
     var expected = this.toString();
-    ok(actual.is(expected), message + ': '+expected+' is not matched');
+    ok(actual.is(expected), message + ': '+expected+' should be matched');
     unmatched = unmatched.not(expected);
   });
 
-  ok(unmatched.length == 0, message + ': matches other elements');
+  ok(unmatched.length == 0, message + ': should not match other elements');
 }
